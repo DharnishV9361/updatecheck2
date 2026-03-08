25361058bbe78b50110113c7ddb83850a34204c3
DharnishV9361 <d9538776@gmail.com>
2026-03-08 17:11:04 +0530

    enable cors for frontend

--- src/main/java/com/dharnish/updatecheck/config/CorsConfig.java
+++ src/main/java/com/dharnish/updatecheck/config/CorsConfig.java
@@ -2,8 +2,7 @@ package com.dharnish.updatecheck.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
@@ -13,6 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
